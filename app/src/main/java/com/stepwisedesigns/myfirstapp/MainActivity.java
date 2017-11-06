341f876875e527c8d2739e7d11c370ac829d2669
SubCipher <kpicart@stepwisedesigns.com>
2017-11-05 23:43:19 -0500

    first android demo app

---
+++ app/src/main/java/com/stepwisedesigns/myfirstapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.stepwisedesigns.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addButton00 = findViewById(R.id.addButton00);
+
+        addButton00.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = findViewById(R.id.secondNumEditText);
+                TextView resultTextView = findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTextView.setText(result + "");
+
+
+
+
+
+            }
+        });
+
+    }
+}
